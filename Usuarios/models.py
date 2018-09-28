61ddb0d7cb1c530d3b7d035d4b6215f3dd833c85
ojag95 <ojag95@gmail.com>
2018-09-28 01:11:41 -0500

    Añadido ejemplo de DjangoRestFramework

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellidoPaterno=models.CharField(max_length=50)
+    apellidoMaterno=models.CharField(max_length=50)
+    fechaNacimiento=models.DateField()
+    email=models.EmailField()
+    usuario=models.CharField(max_length=50,primary_key=True)
+    domicilio=models.TextField()
